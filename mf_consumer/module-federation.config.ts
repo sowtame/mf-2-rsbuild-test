1578bb3c88daf4e6d922c40a082d9139d01d1510
u_m1mkt <sowtame@gmail.com>
2025-05-12 17:27:01 +0700

    init

--- mf_consumer/module-federation.config.ts
+++ mf_consumer/module-federation.config.ts
@@ -2,9 +2,9 @@ import { createModuleFederationConfig } from '@module-federation/rsbuild-plugin'
 
 export default createModuleFederationConfig({
   name: 'mf_consumer',
-  remotes: {
-    provider: 'rslib_provider@http://localhost:3001/mf-manifest.json ',
-  },
+  remotes: {},
+  dts: false,
+  manifest: false,
   shareStrategy: 'loaded-first',
   shared: {
     react: { singleton: true },
--- mf_provider/module-federation.config.ts
+++ mf_provider/module-federation.config.ts
@@ -5,7 +5,10 @@ export default createModuleFederationConfig({
   exposes: {
     '.': './src/components/ProviderComponent.tsx',
   },
+  filename: 'remoteEntry.js',
   getPublicPath: `function() {return "https:" + "/resource/app/"}`,
+  dts: false,
+  manifest: false,
   shared: {
     react: { singleton: true },
     'react-dom': { singleton: true },
